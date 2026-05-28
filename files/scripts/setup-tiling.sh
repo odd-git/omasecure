d4147467952fc8c4e89e62ee484a2389e934a46c
odd-git <oddgit@tuta.com>
2026-05-28 17:47:00 +0200

    modify scritp

--- files/scripts/setup-tiling.sh
+++ files/scripts/setup-tiling.sh
@@ -3,6 +3,17 @@
 set -euo pipefail
 ## DNF5 Speedup
 sed -i '/^\[main\]/a max_parallel_downloads=10' /etc/dnf/dnf.conf
+dnf -y install libvirt virt-manager qemu-kvm flatpak-builder wlr-randr iotop sysstat lxqt-openssh-askpass lxpolkit parallel
+
+# User apps
+dnf -y install nautilus kitty mpv gnome-terminal gnome-system-monitor
+
+# Nautilus open any terminal extension
+curl -Lo /etc/yum.repos.d/nautilus-open-any-terminal.repo \
+  https://copr.fedorainfracloud.org/coprs/monkeygold/nautilus-open-any-terminal/repo/fedora-$(rpm -E %fedora)/monkeygold-nautilus-open-any-terminal-fedora-$(rpm -E %fedora).repo
+dnf install -y nautilus-open-any-terminal
+glib-compile-schemas /usr/share/glib-2.0/schemas
+gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal kitty
 
 # Install Dank Linux shell
 curl --output-dir "/etc/yum.repos.d/" \
@@ -26,3 +37,17 @@ mkdir -p /etc/skel/.config/systemd/user/graphical-session.target.wants
 ln -s /usr/lib/systemd/user/dms.service /etc/skel/.config/systemd/user/graphical-session.target.wants/
 mkdir -p /etc/skel/.config/niri/
 cp -rf /tmp/files/dot_config/niri/config.kdl /etc/skel/.config/niri/
+
+#### Enable podman
+systemctl enable podman.socket
+
+# Disable Origami tips
+sudo mv /etc/profile.d/origami-aliases.sh /etc/profile.d/origami-aliases.sh.bak
+# Remove COSMIC shell and waybar
+dnf -y remove cosmic-comp cosmic-initial-setup cosmic-settings cosmic-settings-daemon cosmic-store waybar
+
+## CLEAN UP
+# Clean up dnf cache to reduce image size
+dnf5 -y clean all
+rm -rf /run/dnf /run/selinux-policy
+rm -rf /var/lib/dnf
