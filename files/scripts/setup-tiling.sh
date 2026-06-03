79be2bec53d6d4305d72f0922651d502ca2b4978
odd-git <oddgit@tuta.com>
2026-06-03 16:20:55 +0200

    fix script

--- files/scripts/setup-tiling.sh
+++ files/scripts/setup-tiling.sh
@@ -6,10 +6,10 @@ set -ouex pipefail
 sed -i '/^\[main\]/a max_parallel_downloads=10' /etc/dnf/dnf.conf
 
 ## System apps
-dnf -y install libvirt virt-manager qemu-kvm flatpak-builder wlr-randr iotop sysstat lxqt-openssh-askpass lxpolkit parallel
+dnf -y install libvirt virt-manager qemu-kvm flatpak-builder wlr-randr iotop sysstat lxqt-openssh-askpass lxpolkit parallel just
 
 # User apps
-dnf -y install nautilus kitty mpv gnome-terminal gnome-system-monitor
+dnf -y install nautilus kitty mpv gnome-terminal gnome-system-monitor gnome-calculator
 
 # OBS and fully-featured ffmpeg with nonfree components from rpm fusion
 dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
@@ -18,12 +18,12 @@ dnf -y install ffmpeg x264-libs obs-studio obs-studio-plugin-x264 --allowerasing
 # Nautilus open any terminal extension
 curl -Lo /etc/yum.repos.d/nautilus-open-any-terminal.repo \
   https://copr.fedorainfracloud.org/coprs/monkeygold/nautilus-open-any-terminal/repo/fedora-$(rpm -E %fedora)/monkeygold-nautilus-open-any-terminal-fedora-$(rpm -E %fedora).repo
-dnf install -y nautilus-open-any-terminal
-glib-compile-schemas /usr/share/glib-2.0/schemas
-gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal kitty
+#dnf install -y nautilus-open-any-terminal
+#glib-compile-schemas /usr/share/glib-2.0/schemas
+#gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal kitty
 
 # Install Niri
-dnf -y install niri
+dnf -y install niri bibata-cursor-theme
 
 # # Install Noctalia shell
 # curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo -o /etc/yum.repos.d/terra.repo
@@ -63,12 +63,8 @@ cp -rf /tmp/files/dot_config/niri/config.kdl /etc/skel/.config/niri/
 
 systemctl enable podman.socket
 
-# Disable Origami tips
-
-sudo mv /etc/profile.d/origami-aliases.sh /etc/profile.d/origami-aliases.sh.bak
-
-# Remove COSMIC shell and waybar
-dnf -y remove cosmic-comp cosmic-initial-setup cosmic-settings cosmic-settings-daemon cosmic-store waybar
+# Remove useless
+dnf -y remove waybar
 
 ## CLEAN UP
 # Clean up dnf cache to reduce image size
